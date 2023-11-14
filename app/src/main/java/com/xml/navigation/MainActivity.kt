7ff29e1a7039ac1786bf9522196843dfe36c7a6a
Diego <diego.rof@hotmail.com>
2023-11-14 12:15:48 -0300

    Navegação com intent de Main pra Home

--- app/src/main/java/com/xml/navigation/MainActivity.kt
+++ app/src/main/java/com/xml/navigation/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.xml.navigation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btNavigate: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btNavigate = findViewById(R.id.bt_navigate)
+
+        btNavigate.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

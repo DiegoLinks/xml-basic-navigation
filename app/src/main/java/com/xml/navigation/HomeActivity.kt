28345336681e90972a560b58aa7a102a006a83e2
Diego <diego.rof@hotmail.com>
2023-11-16 17:57:33 -0300

    Layouts para passagem e exibição do objeto user

--- app/src/main/java/com/xml/navigation/HomeActivity.kt
+++ app/src/main/java/com/xml/navigation/HomeActivity.kt
@@ -7,16 +7,18 @@ import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
 
-    lateinit var tvLabel: TextView
+    lateinit var tvName : TextView
+    lateinit var tvAge : TextView
+    lateinit var tvSubscribe : TextView
+    lateinit var tvScore : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        tvLabel = findViewById(R.id.tv_label)
-
-        val label = intent.getStringExtra("label") ?: "vazio"
-
-        tvLabel.text = label
+        tvName = findViewById(R.id.tv_name)
+        tvAge = findViewById(R.id.tv_age)
+        tvSubscribe = findViewById(R.id.tv_subscribe)
+        tvScore = findViewById(R.id.tv_score)
     }
 }
--- app/src/main/java/com/xml/navigation/MainActivity.kt
+++ app/src/main/java/com/xml/navigation/MainActivity.kt
@@ -9,20 +9,15 @@ import android.widget.EditText
 class MainActivity : AppCompatActivity() {
 
     lateinit var btNavigate: Button
-    lateinit var edLabel: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btNavigate = findViewById(R.id.bt_navigate)
-        edLabel = findViewById(R.id.ed_label)
-
-        val label = edLabel.text
 
         btNavigate.setOnClickListener {
             val intent = Intent(this, HomeActivity::class.java)
-            intent.putExtra("label", label.toString())
             startActivity(intent)
         }
     }
